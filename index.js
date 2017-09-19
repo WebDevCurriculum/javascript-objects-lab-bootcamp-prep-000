3a53d0f20d66bd7124473e87735ca34b79fe6a1d
Charity Youngblood <youngblood.charity@gmail.com>
2017-09-19 19:29:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object,key){
-  delete object.key
-  return Object.assign({}, object)
+  var newObject = delete object.key
+  return newObject
 }
